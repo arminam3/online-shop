37beb6feac433f960b9221f25518c573688c704e
unknown <armin.tree@gmail.com>
2022-09-11 15:04:32 +0430

    implement cart 227

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,5 @@
+from products.models import Product
+
 
 class Cart:
     def __init__(self, request):
@@ -28,10 +30,49 @@ class Cart:
 
         self.save()
 
+    def remove(self, product):
+        """
+        Remove a product from cart
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+
+        self.save()
+
+
     def save(self):
         """
         Save Changes In Session
         """
         self.session.modified = True
 
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
 
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product_obj'] = product
+
+        for item in cart.values():
+            yield item
+
+    def __len__(self):
+        return len(self.cart.keys())
+
+    def clear(self):
+        del self.session['cart']
+        self.save()
+
+    def get_total_price(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        # total = 0
+        #
+        # for product in products:
+        #     quantity = self.cart[str(product.id)]['quantity']
+        #     total += product.price * quantity
+        # return total
+        return sum(product.price for product in products)
